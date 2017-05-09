22179573bd98ab56a2b85e522fc9129f353a8c68
Fire Meditation <firemeditation@gmail.com>
2017-05-09 08:50:51 +0800

    add drulemanager

---
+++ drulemanager/door.go
@@ -0,0 +1,25 @@
+// Copyright 2016-2017
+// CoderG the 2016 project
+// Insight 0+0 [ 洞悉 0+0 ]
+// InDimensions Construct Source [ 忆黛蒙逝·建造源 ] -> idcsource@gmail.com
+// Stephen Fire Meditation Qin [ 火志溟 ] -> firemeditation@gmail.com
+// Use of this source code is governed by GNU LGPL v3 license
+
+// Drule Manager
+//
+// 这是一个符合Insight 0+0 webs2规范的站点单元，但不包括js、css、template等静态文件
+package drulemanager
+
+import (
+	"github.com/idcsource/Insight-0-0-lib/webs2"
+)
+
+// drulemanager的入口
+type Door struct {
+}
+
+func (d *Door) FloorList() (floors webs2.FloorDoor) {
+	floors = make(map[string]webs2.FloorInterface)
+
+	return
+}
